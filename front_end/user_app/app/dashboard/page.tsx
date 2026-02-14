d399360c19f9450220f88ad252264d9443512a8d
Xzura <109458343+raihantririzqi@users.noreply.github.com>
2026-02-14 20:03:57 +0700

    Update page.tsx

--- front_end/user_app/app/dashboard/page.tsx
+++ front_end/user_app/app/dashboard/page.tsx
@@ -92,7 +92,7 @@ export default function DashboardPage() {
 
     const displayUser = user || { name: "User", nim: "000000000", profile_picture: "" };
 
-    const TARGET_DATE = new Date("2026-01-19T21:36:00").getTime();
+    const TARGET_DATE = new Date("2026-02-17T12:35:00").getTime();
 
     useEffect(() => {
         // Prioritas 1: Jika sudah voting, langsung set VOTED
